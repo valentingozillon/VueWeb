23b5105526675179ffcf06211ba8684b99987768
valentingozillon <72022931+valentingozillon@users.noreply.github.com>
2026-02-21 23:15:47 +0100

    :sparkles: page : starting home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template> 
-    <h1>Test</h1>
+    <div class="container">
+        <iframe
+            src="https://www.youtube.com/embed/1AiToLn3USQ"
+            title="YouTube video player"
+            frameborder="0"
+            allowfullscreen
+            class="video">
+        </iframe>
+    </div>
 </template>
 
 <script>
@@ -7,3 +15,19 @@ export default {
     name: 'HomePage'
 }
 </script>
+
+<style scoped>
+
+.container {
+    width: 90%;
+    margin-top: 50px;
+    background-color: #4A919E;
+    margin-left: 5%;
+}
+
+.video {
+    margin-top: 50px;
+    padding-left: 10%;
+}
+
+</style>

43a82651a119fe6cc4bb06e881744d76f02f2694
valentingozillon <72022931+valentingozillon@users.noreply.github.com>
2026-02-23 12:56:19 +0100

    ✨ local JSON: reader with table data using prime vue and TAG

---
+++ src/components/json.vue
@@ -0,0 +1,64 @@
+<template>
+  <DataTable :value="data" class="marg">
+    <template #header>
+        <div>
+            <h2 style="text-align: center;">Tâches</h2>
+        </div>
+    </template>
+    <Column field="type" header="Type"></Column>
+    <Column field="status" header="Status">
+      <template #body="slotProps">
+        <Tag :value="slotProps.data.status" :severity="getSeverity(slotProps.data.status)" />
+      </template>
+    </Column>
+    <template #footer> Au total le site aura {{ data ? data.length : 0 }} tâches. </template>
+</DataTable>
+</template>
+
+<script>
+import { Column, DataTable, Tag } from "primevue";
+import userData from "../assets/learn.json"
+
+
+export default {
+
+  name: 'json-inf',
+  props: {
+    Path: String
+  },
+  components: {
+    Column,
+    DataTable,
+    Tag
+  },
+  data() {
+    return {data: userData}
+  },
+  methods: {
+    getSeverity(status) {
+            switch (status) {
+                case 'finis':
+                    return 'success';
+
+                case 'en cours':
+                    return 'warn';
+
+                case 'futur':
+                    return 'danger';
+
+                default:
+                    return null;
+            }
+        }
+  }
+}
+</script>
+
+<style scoped>
+
+.marg {
+  margin-bottom: 50px;
+  background-color: #CE6A6B;
+}
+
+</style>
